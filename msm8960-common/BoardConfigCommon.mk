8f18bf10caeeb38586fb46f78d791e579f0abb43
Linh Phi <linhphi9x94@gmail.com>
2016-10-05 04:21:07 +0700

    audio: msm8960 use AUDIO_USE_LL_AS_PRIMARY_OUTPUT

--- msm8960-common/BoardConfigCommon.mk
+++ msm8960-common/BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2016 XDAVN
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -73,6 +73,7 @@ OVERRIDE_RS_DRIVER 				:= libRSDriver_adreno.so
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
+AUDIO_USE_LL_AS_PRIMARY_OUTPUT := true
 USE_CUSTOM_AUDIO_POLICY := 1
 
 # QCOM enhanced A/V
