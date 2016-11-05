579579db051b02d2be499da9e94565e9ac38bd0d
Le Hoang <icy.lord.love.angel@gmail.com>
2016-11-05 20:43:14 +0700

    Add bluetooth props

--- msm8960-common/system_prop.mk
+++ msm8960-common/system_prop.mk
@@ -116,3 +116,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.ringtone=Orion.ogg
+
+PRODUCT_PROPERTY_OVERRIDES += \
+      qcom.bluetooth.soc=smd \
+      ro.qualcomm.bt.hci_transport=smd \
+      ro.bt.bdaddr_path=/data/misc/bluetooth/bdaddr
